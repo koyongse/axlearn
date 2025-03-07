11d6337454034d7df050e1588b813bbf57495741
Yongseok Koh <koyongse@amazon.com>
2025-03-07 07:17:05 +0000

    Use batch size 1 per TP group for performance testing
    Signed-off-by: Yongseok Koh <koyongse@amazon.com>

--- axlearn/experiments/text/gpt/fuji.py
+++ axlearn/experiments/text/gpt/fuji.py
@@ -10,6 +10,7 @@ The fuji models are set up to imitate LLaMA models:
 * LLaMA 3: https://github.com/meta-llama/llama3
 """
 
+import jax
 import enum
 import functools
 import itertools
@@ -658,7 +659,7 @@ def get_trainer_kwargs(
             ),
             learner_kwargs=dict(peak_lr=1.5e-4, weight_decay=0.1),
             max_sequence_length=max_sequence_length,
-            train_batch_size=train_batch_size,
+            train_batch_size=int(len(jax.devices())/4),
             max_step=max_step,
             mesh_shape=mesh_shape_from_axes(fsdp=-1),
             mesh_rules=(
